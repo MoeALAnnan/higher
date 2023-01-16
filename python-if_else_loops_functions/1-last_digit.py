6ec316640a4507c4747c8dce0b0001e72484ffcf
MoeAlAnnan <5653@holbertonstudents.com>
2023-01-16 13:08:05 -0800

    fixing type the last > Last

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-check = abs(number%10)
+check = abs(number % 10)
 if number > 0:
-	if check > 5:
-		print(f"the last digit of {number} is {check} and is greater than 5")
-	elif check == 0:
-		print(f"the last digit of {number} is {check} and is 0")
-	elif check < 6 and check != 0:
-		print(f"the last digit of {number} is {check} and is less than 6 and not 0")
+    if check > 5:
+        print(f"Last digit of {number} is {check} and is greater than 5")
+    elif check == 0:
+        print(f"Last digit of {number} is {check} and is 0")
+    elif check < 6 and check != 0:
+        print(f"Last digit of {number} is {check}",
+              "and is less than 6 and not 0")
+
 elif number < 0:
-	number = number * -1
-	check = number%10
-	if check == 0:
-                print(f"the last digit of -{number} is {check} and is 0")
-	else:
-		print(f"the last digit of -{number} is -{check} and is less than 6 and not 0")
+    number = number * -1
+    check = number % 10
+    if check == 0:
+        print(f"Last digit of -{number} is {check} and is 0")
+    else:
+        print(f"Last digit of -{number} is -{check}",
+        "and is less than 6 and not 0")
+else:
+    print(f"Last digit of {number} is {check} and is 0")
