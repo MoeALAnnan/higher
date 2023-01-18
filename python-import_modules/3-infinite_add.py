7b7e9747daab8fb0a118bb6f9ad82ca6a34d8c54
MoeAlAnnan <5653@holbertonstudents.com>
2023-01-18 04:43:06 -0800

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    y = 0
+    if (len(argv) > 1):
+        for x in range(1, len(argv)):
+            y = y + int(argv[x])
+        print("{}".format(y))
