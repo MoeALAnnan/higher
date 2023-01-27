45150f48bd2aac397f68eed2ab3a7edbc3fd9c36
MoeAlAnnan <5653@holbertonstudents.com>
2023-01-27 04:37:27 -0800

    fixing number of charachters by print()

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,5 @@ def print_matrix_integer(matrix=[[]]):
         for x in matrix:
             for y in range(len(x)):
                 print("{:d}".format(x[y]), end=" ")
-            print("")
+            print()
+
