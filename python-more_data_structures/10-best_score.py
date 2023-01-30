d1cfe69199fc64f310ebe94f05865a2ef799d587
MoeAlAnnan <5653@holbertonstudents.com>
2023-01-30 05:05:33 -0800

    fixing sorting by value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return (None)
-    for k, v in sorted(a_dictionary.items()):
-        new_key = k
+    for v in sorted(a_dictionary.values()):
+        max_value = v
+    for k in a_dictionary:
+        new_k = k
     return (k)
