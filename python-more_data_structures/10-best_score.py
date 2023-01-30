727ad89e5b77d17b2f2cef3ead48a74e5640b404
MoeAlAnnan <5653@holbertonstudents.com>
2023-01-30 03:41:09 -0800

    10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return (None)
+    for k, v in sorted(a_dictionary.items()):
+        new_key = k
+    return (k)
