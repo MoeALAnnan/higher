989a767a638b59ee8fc118520b8ae0c48462e4e6
MoeAlAnnan <5653@holbertonstudents.com>
2023-01-31 05:36:01 -0800

    trying x += 0 to check for int

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    try:
-        if value is int:
+    try:    
+            value += 0
             print("{:d}".format(value))
-            return True
+            return (True)
     except TypeError:
         return (False)
