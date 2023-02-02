300fa572e2451d671fc7e3f7033fbf6c04209b6e
MoeAlAnnan <5653@holbertonstudents.com>
2023-02-02 08:46:32 -0800

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" this is a class the defines a square """
+
+
+class Square:
+    """ declaring a private instance attribute """
+
+    __size = None
+
+    def __init__(self, size=0):
+        """Initializes the data."""
+        self.__size = size
+        x = isinstance(self.__size, int)
+        if x is False:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            pass
