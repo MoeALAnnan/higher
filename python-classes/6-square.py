a1e63780c57c13ea8aecdbcbf595e924391a9fca
MoeAlAnnan <5653@holbertonstudents.com>
2023-02-03 03:28:21 -0800

    6-square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+""" this is a class the defines a square """
+
+
+class Square:
+    """ declaring a private instance attribute """
+
+    __size = None
+    __position = None
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the data."""
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """method to return area"""
+        return self.__size**2
+
+    @property
+    def size(self):
+
+        """retrieving size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        """managing values"""
+
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """a method that prints squares
+        using hashtaghs depending on the size"""
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
+            
+    @property
+    def position(self):
+
+        """ retrieving position """
+        return self.__position
+    @position.setter
+    def position(self, value):
+
+        """ setting position """
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2 or value[0] < 0 or value[1] <0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
