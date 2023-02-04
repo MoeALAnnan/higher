d0a676848faecbc4ff7eb1bf328d8f24ec4e30f1
MoeAlAnnan <5653@holbertonstudents.com>
2023-02-04 07:31:26 -0800

    fixing type error removing space in line 64 after Rectangle

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -63,4 +63,4 @@ class Rectangle:
         return z
 
     def __repr__(self):
-        return "Rectangle ({}, {})".format(self.__width, self.__height)
+        return "Rectangle({}, {})".format(self.__width, self.__height)
