0b3c7c3409b4bd6015b8ffb69bad65eaff752c20
MoeAlAnnan <5653@holbertonstudents.com>
2023-02-06 05:58:13 -0800

    adding doc

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" a function that prints 2 strings """
+
+
 def say_my_name(first_name, last_name=""):
+    """ a function that prints 2 strings """
     try:
         print("My name is {:s} {:s}".format(first_name, last_name))
     except:
