b32f6efafe67e366fa3683a968db7b12ba1a3787
MoeAlAnnan <5653@holbertonstudents.com>
2023-02-06 00:51:41 -0800

    0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a function that adds two intgers """
+
+def add_integer(a, b=98):
+    """ a function that adds two intgers """
+    if isinstance(a, int) or isinstance(a, float):
+        if type(a) is float:
+            a = int(a)
+    else:
+        raise TypeError("a must be an integer")
+
+    if isinstance(b, int) or isinstance(b, float):
+        if type(b) is float:
+            b = int(b)
+    else:
+        raise TypeError("b must be an integer")
+    return (a + b)
