bd9bb16be7646e3960a20ee9b2d56c5c4b30952a
MoeAlAnnan <5653@holbertonstudents.com>
2023-02-09 13:51:05 -0800

    adding documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """ a function that reads file and print it to the terminal """
     with open(filename, 'r', encoding="utf-8") as f:
         for x in f:
             print("{}".format(x), end="")
