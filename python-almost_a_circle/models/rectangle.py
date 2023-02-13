b2847efa68af5b3e2685cd69393f9f0fec2fa86c
MoeAlAnnan <5653@holbertonstudents.com>
2023-02-13 07:15:46 -0800

    fixing error message

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,29 +11,17 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         Base.__init__(self, id)
         self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        if type(self.__width) is not int:
+        if type(width) is not int:
             raise TypeError("width must be an integer")
-        elif type(self.__height) is not int:
+        self.__height = height
+        if type(height) is not int:
             raise TypeError("height must be an integer")
-        elif type(self.__x) is not int:
+        self.__x = x
+        if type(x) is not int:
             raise TypeError("x must be an integer")
-        elif type(self.__y) is not int:
+        self.__y = y
+        if type(y) is not int:
             raise TypeError("y must be an integer")
-        elif type(self.__id) is not int:
-            raise TypeError("id must be an integer")
-        if self.__width < 0:
-            raise ValueError("width must be > 0")
-        elif self.__height < 0:
-            raise ValueError("height must be > 0")
-        elif self.__x < 0:
-            raise ValueError("x must be >= 0")
-        elif self.__y < 0:
-            raise ValueError("y must be >= 0")
-        elif self.__id < 0:
-            raise ValueError("id must be >= 0")
 
     @property
     def width(self):
