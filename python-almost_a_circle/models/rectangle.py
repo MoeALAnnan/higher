313db8ea6bb31713cd1a9c77c742480f4c213e36
MoeAlAnnan <5653@holbertonstudents.com>
2023-02-13 08:57:45 -0800

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -104,5 +104,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """magic method"""
-        return ("[Rectangle] ({})  {}/{} - {}/{}".format
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format
                 (self.id, self.__x, self.__y, self.__width, self.__height))
