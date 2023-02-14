a5b3fc6f9a40ff0e6eabd577dad777111d80caa6
MoeAlAnnan <5653@holbertonstudents.com>
2023-02-14 07:10:05 -0800

    square.py getter and setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,9 +10,22 @@ class Square(Rectangle):
     """class Square that imports from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """new init"""
-        super().__init__(size, size, x, y, id)
+        Rectangle.__init__(self, size, size, x, y, id)
 
     def __str__(self):
         """ overloading of str method """
         return ("[Square] ({}) {}/{} - {}".
                 format(self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        return (self.width)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = value
