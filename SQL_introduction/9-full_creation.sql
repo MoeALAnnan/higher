757d13c2d1177eb43529f4fa80819abc74fce87e
MoeAlAnnan <5653@holbertonstudents.com>
2023-03-23 14:19:22 -0700

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- create a second table
-CREATE second_table IF NOT EXISTS(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) 
 id (1, 2, 3, 4) 
 name('JOHN', 'ALEX', 'BOB', 'GEORGE') 
-score (10, 3, 14, 8)
+score (10, 3, 14, 8);
