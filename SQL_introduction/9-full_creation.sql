34cffe921b56abae5cd22fe68abf8a05685da22d
MoeAlAnnan <5653@holbertonstudents.com>
2023-03-23 14:26:51 -0700

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) 
 VALUES
-(1, 'JOHN', 10),
-(2, 'ALEX', 3),
-(3, 'BOB', 14),
-(4, 'GEORGE', 8);
+(♥, 'samoura',♥ ),
+(♥, 'is',♥ ),
+(♥, 'the',♥),
+(♥, 'best',♥);
