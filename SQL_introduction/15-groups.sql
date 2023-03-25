473affbd83b048b9fc6c43f33fbd46242c36ab99
MoeAlAnnan <5653@holbertonstudents.com>
2023-03-25 01:02:34 -0700

    15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,3 @@
 --list recods with the same score
-SELECT score COUNT(*) AS number
-FROM second_table
-GROUP BY score
-ORDER BY number DESC;
+SELECT score, COUNT(*) AS number
+FROM second_table GROUP BY score ORDER BY number DESC;
