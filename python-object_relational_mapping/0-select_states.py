99f2d27fc96b90c4642937a2c536eeb66c194a55
MoeAlAnnan <5653@holbertonstudents.com>
2023-04-05 10:16:30 -0700

    documenting

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,13 +4,15 @@
 import sys
 import MySQLdb
 
+
 # Get the database connection details from command-line arguments
 user = sys.argv[1]
 password = sys.argv[2]
 db_name = sys.argv[3]
 
 # Connect to the MySQL server
-db = MySQLdb.connect(host='localhost', port=3306, user=user, passwd=password, db=db_name)
+db = MySQLdb.connect(host='localhost', port=3306,
+                     user=user, passwd=password, db=db_name)
 
 # Create a cursor object
 cur = db.cursor()
