f53fc30f09b5148f23ac22c9c1183b98d80e63a1
MoeAlAnnan <5653@holbertonstudents.com>
2023-04-08 09:01:25 -0700

    try to fix the documentation

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -25,11 +25,10 @@ if __name__ == '__main__':
     )
 
     cur = db.cursor()
-    cur.execute("""
-        SELECT * FROM states
-        WHERE name LIKE 'N%' COLLATE utf8_bin
-        ORDER BY id ASC
-    """)
+    cur.execute("
+                SELECT * FROM states
+                WHERE name LIKE 'N%' COLLATE utf8_bin
+                ORDER BY id ASC")
 
     rows = cur.fetchall()
     for row in rows:
