af5890917a224becf70798c05feed92f58a2b653
MoeAlAnnan <5653@holbertonstudents.com>
2023-04-10 09:58:47 -0700

    fidxing errors

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=username, passwd=password, db=db_name)
     cur = db.cursor()
-    query = "SELECT id, name, state FROM cities ORDER BY id ASC"
+    query = "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
