fcda553eaa88476a503d75392de8e016847bdbb5
MoeAlAnnan <5653@holbertonstudents.com>
2023-05-02 02:57:01 -0700

    extra space before 1: Hello removed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) == 2:
-        print(f"1 argument: \n1: {argv[1]}")
+        print(f"1 argument:\n1: {argv[1]}")
     elif len(argv) >= 2:
         x = len(argv) - 1
         print(f"{x} arguments:")
