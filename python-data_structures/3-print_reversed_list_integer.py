aa479f34086f077fd4c5bff85fb244f035d72584
MoeALAnnan <113625144+MoeALAnnan@users.noreply.github.com>
2023-05-04 07:43:44 +0200

    Delete python-data_structures directory

--- python-data_structures/3-print_reversed_list_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list is None:
-        return
-    else:
-        reverse = len(my_list)
-        for x in reversed(range(0, reverse)):
-            print("{:d}".format(my_list[x]))
