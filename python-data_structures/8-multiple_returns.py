d62637db46c0bd3e2c9d91f0ddf5845815fb37ca
MoeAlAnnan <5653@holbertonstudents.com>
2023-05-04 05:57:54 -0700

    typing mistake at line 5 comma next to 0

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if sentence is not None:
         if len(sentence) == 0:
-            x = 0,
+            x = 0
             y = None
         else:
             x = len(sentence)
