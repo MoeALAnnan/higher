929f8afef10b134c85264eef6dccad84fa38eb06
MoeAlAnnan <5653@holbertonstudents.com>
2023-05-04 02:50:31 -0700

    fixing iterating while modifiying list problem with list comprehension

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string is not None:
-        my_list = list(my_string)
-        for x in my_list:
-            if x == 'c' or x == 'C':
-                my_list.remove(x)
-        new_string = ''.join(my_list)
-        return (new_string)
+    my_list = list(my_string)
+    new_list = [x for x in my_list if x != 'c' and x != 'C']
+    new_string = ''.join(new_list)
+    return new_string
