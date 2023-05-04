a7e05872412e19848fa4c7e8dd50744355680868
MoeAlAnnan <5653@holbertonstudents.com>
2023-05-04 06:26:02 -0700

    9. Find the max - return None if empty

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,5 +1,8 @@
 #!/bin/usr/python3
 def max_integer(my_list=[]):
     if list is not None:
-        new_list = sorted(my_list)
-        return (new_list[-1])
+        if len(list) = 0:
+            return (None)
+        else:
+            new_list = sorted(my_list)
+            return (new_list[-1])
