2b459c2f1f1d8ded6434f66e58eec3e6d9e05f0d
MoeAlAnnan <5653@holbertonstudents.com>
2023-05-05 08:53:41 -0700

    6-print_sorted_dictionary.py

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_dic = dict(sorted(a_dictionary.items()))
+    for k, v in sorted_dic.items():
+        print("{:s}: {}".format(k, v))
