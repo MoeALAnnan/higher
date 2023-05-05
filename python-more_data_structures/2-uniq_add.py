a219ed3b2aa2e05be51348203cccb36b57b2de55
MoeALAnnan <113625144+MoeALAnnan@users.noreply.github.com>
2023-05-05 07:28:27 +0200

    Delete python-more_data_structures directory

--- python-more_data_structures/2-uniq_add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def uniq_add(my_list=[]):
-    my_list.sort()
-    y = 0
-    for x in range(len(my_list)):
-        if my_list[x] != my_list[-1]:
-            if my_list[x] != my_list[x+1]:
-                y = y + my_list[x]
-    return (y + my_list[-1])
