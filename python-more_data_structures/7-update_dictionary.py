fc146019d7922bd41c1f732427e778c3a6d960d7
MoeAlAnnan <5653@holbertonstudents.com>
2023-05-09 01:18:35 -0700

    7-update_dictionary.py

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    for k in a_dictionary.keys():
+        if k == key:
+            a_dictionary[key] = value
+        else:
+            break
+    a_dictionary[key] = value
+    return(a_dictionary)
