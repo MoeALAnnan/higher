956baa7de603818886e1f7c1b38919167793ed41
MoeAlAnnan <5653@holbertonstudents.com>
2023-05-09 01:26:43 -0700

    removing the else statement

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,7 +3,5 @@ def update_dictionary(a_dictionary, key, value):
     for k in a_dictionary.keys():
         if k == key:
             a_dictionary[key] = value
-        else:
-            break
     a_dictionary[key] = value
-    return(a_dictionary)
+    return (a_dictionary)
