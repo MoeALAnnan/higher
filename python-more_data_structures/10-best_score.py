f456efb790d3c116566beaa0c0ed514571d71125
MoeAlAnnan <5653@holbertonstudents.com>
2023-05-09 03:00:28 -0700

    10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        new_dictionary = dict(sorted(a_dictionary.items(), key=lambda x: x[1]))
+        return (new_dictionary.popitem()[0])
