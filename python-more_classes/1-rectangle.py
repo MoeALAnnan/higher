e5748a52a92c8e6ff388a0c42bfba31f65496e37
MoeAlAnnan <5653@holbertonstudents.com>
2023-05-14 10:19:30 -0700

    adding exception in init

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,6 +7,15 @@
 class Rectangle:
     """ Class that defines a Rectangle """
     def __init__(self, width=0, height=0):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        elif type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
         self.__width = width
         self.__height = height
 
