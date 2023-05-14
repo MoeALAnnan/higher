13363f5f55dbb92d08447046b3bd138224a21afb
MoeAlAnnan <5653@holbertonstudents.com>
2023-05-14 01:16:52 -0700

    0-rectangle.py

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python3
-""" this is a class the defines a Rectangle """
+""" This will be an excercise
+    to practice class and objects
+"""
 
 
 class Rectangle:
-     """Initializes the data."""
-    def __init__(self, width=0, height=0):
-         """Initializes the data."""
-        self.width = width
-        self.height = height
-    
-    @property
-        def width(self)
-        """retrieving width"""
-            return self.__width
-    @width.setter
-        def width(self, value)
-        """managing the width"""
-            if isinstance(value, int) is False:
-                raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self.__width = value
-
+    """ Class that defines a Rectangle """
+    pass
