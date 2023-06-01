fe6dd7774adcf738e8861c056706470fb2b36fa8
MoeAlAnnan <5653@holbertonstudents.com>
2023-06-01 11:20:08 -0700

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,13 +7,13 @@ class Rectangle(BaseGeometry):
     """ class Rectangle inheriting from BaseGeometry """
     def __init__(self, width, height):
         if width <= 0:
-            raise TypeError("width must be greater than 0")
+            raise ValueError("width must be greater than 0")
         elif height <= 0:
-            raise TypeError("height must be greater than 0")
+            raise ValueError"height must be greater than 0")
         elif type(width) is not int:
-            raise ValueError("width must be an integer")
+            raise TypeError("width must be an integer")
         elif type(height) is not int:
-            raise ValueError("height must be an integer")
+            raise TypeError("height must be an integer")
         else:
             self.__width = width
             self.__height = height
