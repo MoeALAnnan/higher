b5609d9433f900e08f74811bbfe66787f88ade29
MoeAlAnnan <5653@holbertonstudents.com>
2023-06-10 04:13:39 -0700

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" write files"""
+""" a function that write a file """
 
 
 def write_file(filename="", text=""):
-    """ a function that reads file and print it to the terminal """
-    # we used a+ to create a file that if it does not exist
-    with open(filename, 'a+', encoding="utf-8") as f:
-        return (f.write(text))
+    """write and return number of charachters """
+    with open(filename, 'w', encoding="utf-8") as f:
+        write = f.write(text)
+        return (write)
