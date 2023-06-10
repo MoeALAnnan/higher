299d4784edd29fb2c46787bdb09eb5d5994a6915
MoeAlAnnan <5653@holbertonstudents.com>
2023-06-10 02:24:40 -0700

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-""" opening files """
-
-
 def read_file(filename=""):
-    """ a function that reads file and print it to the terminal """
     with open(filename, 'r', encoding="utf-8") as f:
-        for x in f:
-            print("{}".format(x), end="")
+        read = f.read()
+        print(read, end="")
