8ee04774af4fbe8d229cb9dd1d5b83b3011d0962
MoeAlAnnan <5653@holbertonstudents.com>
2023-06-10 02:26:10 -0700

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" a function that read a file """
+
+
 def read_file(filename=""):
+    """read and print it to std out """
     with open(filename, 'r', encoding="utf-8") as f:
         read = f.read()
         print(read, end="")
