2afd634b589f517c4a877873aa96b04518d38170
MoeAlAnnan <5653@holbertonstudents.com>
2023-06-11 03:00:36 -0700

    6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-""" load object from json file"""
+""" import """
 import json
+""" a function that deserialize """
 
 
 def load_from_json_file(filename):
-    """ a function that loads  object from json file """
-    with open(filename, encoding="utf-8") as f:
-        y = ""
-        for k in f:
-            y = y + k
-        return (json.loads(y))
+    """ load from file """
+    with open(filename, 'r', encoding='utf-8') as f:
+        deserializing = json.load(f)
+        return (deserializing)
