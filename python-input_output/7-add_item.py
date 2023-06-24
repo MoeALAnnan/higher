7a13280a0b7bdc7ef12fa0c0bd77f404f09eb4d2
MoeAlAnnan <5653@holbertonstudents.com>
2023-06-24 08:54:56 -0700

    7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" import """
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+""" a script that adds arguments to a json file """
+add_args = []
+if len(sys.argv) > 1:
+    for i in range(1, len(sys.argv)):
+        add_args.append(sys.argv[i])
+print(add_args)
+save_to_json_file(add_args, "add_item.json")
