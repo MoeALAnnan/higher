c4f659776c5dc3eeb7fc8deffb3b67b7983936ba
MoeAlAnnan <5653@holbertonstudents.com>
2023-06-24 12:49:12 -0700

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,14 +5,9 @@
 class Student:
     """class student"""
     def __init__(self, first_name, last_name, age):
-        """init method"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """to json method"""
-        dictionary = {'first_name': self.first_name,
-                      'last_name': self.last_name,
-                      'age': self.age}
-        return (dictionary)
+        return (vars(self))
