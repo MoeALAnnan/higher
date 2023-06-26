aeaa7a17dbb8de74df59ccbcd7037dd605b05fc8
MoeAlAnnan <5653@holbertonstudents.com>
2023-06-26 11:56:48 -0700

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -5,23 +5,20 @@
 class Student:
     """class student"""
     def __init__(self, first_name, last_name, age):
-        """init method"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attr=None):
-        """to json method"""
-        new_dic = {}
-        dictionary = {'first_name': self.first_name,
-                      'last_name': self.last_name,
-                      'age': self.age}
-        if type(attr) == list:
-            new_dictionary = {}
-            for i in attr:
-                for k, v in dictionary.items():
-                    if i == k:
-                        new_dictionary[k] = v
-            return (new_dictionary)
+    def to_json(self, attrs=None):
+        my_dict = {}
+        if type(attrs) == list:
+            for i in range(len(attrs)):
+                if attrs[i] == "first_name":
+                    my_dict['first_name'] = self.first_name
+                elif attrs[i] == "last_name":
+                    my_dict['last_name'] = self.last_name
+                elif attrs[i] == "age":
+                    my_dict['age'] = self.age
+            return (my_dict)
         else:
-            return (dictionary)
+            return (vars(self))
